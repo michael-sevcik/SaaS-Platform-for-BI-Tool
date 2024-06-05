e242f7b2971c3cd2cc12a9920cb00399d883fc5d
Michael Ševčík <69696161@cuni.cz>
2024-06-05 02:52:55 +0200

    Add more entities

--- src/Modules/DataIntegration/Domain/SchemaScraping/Column.cs
+++ src/Modules/DataIntegration/Domain/SchemaScraping/Column.cs
@@ -1,6 +1,11 @@
-﻿namespace BIManagement.Modules.DataIntegration.Domain.SchemaScraping
+﻿namespace BIManagement.Modules.DataIntegration.Domain.SchemaScraping;
+
+/// <summary>
+/// Represents a column in a database table - <see cref="Table"/>.
+/// </summary>
+public class Column
 {
-    public class Column
-    {
-    }
+    string Name { get; set; } = string.Empty;
+    // TODO: define a data type 
+    DataType DataType { get; set; } = string.Empty;
 }
---
+++ src/Modules/DataIntegration/Domain/SchemaScraping/DataType.cs
@@ -0,0 +1,24 @@
+﻿namespace BIManagement.Modules.DataIntegration.Domain.SchemaScraping;
+
+public class DataType
+{
+    public enum Simpletype
+    {
+        Integer,
+        Boolean,
+        Decimal,
+        Date,
+        Datetime,
+        Time,
+    }
+
+    public virtual bool IsSimple => true;
+
+    
+}
+
+public class NVarChar : DataType
+{
+    public override bool IsSimple => false;
+    public int lenght { get; set; }
+}
