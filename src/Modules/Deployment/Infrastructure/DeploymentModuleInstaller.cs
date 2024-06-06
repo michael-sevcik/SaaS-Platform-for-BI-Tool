b9e4bb805ee85d12b317a0c3d85030bbd003e2b9
Michael Ševčík <69696161@cuni.cz>
2024-06-06 22:48:20 +0200

    Update solution and add new projects, files, and configurations

--- src/Modules/Deployment/Infrastructure/DeploymentModuleInstaller.cs
+++ src/Modules/Deployment/Infrastructure/DeploymentModuleInstaller.cs
@@ -5,18 +5,14 @@ using BIManagement.Common.Infrastructure.Extensions;
 
 namespace BIManagement.Modules.Deployment.Infrastructure;
 
+/// <summary>
+/// Deployment module installer.
+/// 
+/// Installs the services of the deployment module.
+/// </summary>
 public class DeploymentModuleInstaller : IModuleInstaller
 {
+    /// <inheritdoc/>
     public static void Install(IServiceCollection services, IConfiguration configuration)
-    {
-        //services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
-        services.InstallServicesFromAssemblies(configuration,
-            AssemblyReference.Assembly
-            //  TODO: ADD all ADDITIONAL ASSEMBLIES
-            );
-
-
-        // TODO: FINISH installer
-        //throw new NotImplementedException();
-    }
+        => services.InstallServicesFromAssemblies(configuration, AssemblyReference.Assembly);
 }
---
+++ src/Modules/Deployment/Persistence/Constants/Schemas.cs
@@ -0,0 +1,12 @@
+﻿namespace BIManagement.Modules.Deployment.Persistence.Constants;
+
+/// <summary>
+/// Encapsulates the schema names used in the Deployment module.
+/// </summary>
+public static class Schemas
+{
+    /// <summary>
+    /// The schema name for the Deployment module.
+    /// </summary>
+    public const string Deployment = "deployment";
+}
