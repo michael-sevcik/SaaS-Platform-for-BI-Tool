d1b8a292141849121f1b38e88d7a6692327f494a
Michael Ševčík <69696161@cuni.cz>
2024-06-07 01:05:19 +0200

    Add test project for DbModeling

---
+++ test/Modules/DataIntegration/DbModelling/MSSQLDbModelBuilderTests.cs
@@ -0,0 +1,46 @@
+﻿using BIManagement.Modules.DataIntegration.DbSchemaScraping;
+using BIManagement.Modules.DataIntegration.Domain.DatabaseConnection;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace BIManagement.Test.Modules.DataIntegration.DbModelling
+{
+    [TestFixture]
+    public class MSSQLDbModelBuilderTests
+    {
+        private MockRepository mockRepository;
+
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.mockRepository = new MockRepository(MockBehavior.Strict);
+
+
+        }
+
+        private MSSQLDbModelBuilder CreateMSSQLDbModelBuilder()
+        {
+            return new MSSQLDbModelBuilder();
+        }
+
+        [Test]
+        public async Task CreateAsync_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var mSSQLDbModelBuilder = this.CreateMSSQLDbModelBuilder();
+            DbConnectionConfiguration configuration = null;
+
+            // Act
+            var result = await mSSQLDbModelBuilder.CreateAsync(
+                configuration);
+
+            // Assert
+            Assert.Fail();
+            this.mockRepository.VerifyAll();
+        }
+    }
+}
