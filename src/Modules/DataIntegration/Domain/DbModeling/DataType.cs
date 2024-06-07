e0b597856a5b52de6bfd98c4672b8bf6ac9a1f94
Michael Ševčík <69696161@cuni.cz>
2024-06-07 19:23:15 +0200

    Add column types

--- src/Modules/DataIntegration/Domain/DbModeling/DataType.cs
+++ src/Modules/DataIntegration/Domain/DbModeling/DataType.cs
@@ -8,41 +8,123 @@ namespace BIManagement.Modules.DataIntegration.Domain.DbModelling;
 [JsonDerivedType(typeof(SimpleType), typeDiscriminator: SimpleType.Descriptor)]
 [JsonDerivedType(typeof(NVarChar), typeDiscriminator: NVarChar.Descriptor)]
 [JsonDerivedType(typeof(VarChar), typeDiscriminator: VarChar.Descriptor)]
+[JsonDerivedType(typeof(NVarCharMax), typeDiscriminator: NVarCharMax.Descriptor)]
+[JsonDerivedType(typeof(VarCharMax), typeDiscriminator: VarCharMax.Descriptor)]
+[JsonDerivedType(typeof(UnknownDataType), typeDiscriminator: UnknownDataType.Descriptor)]
 public abstract class DataTypeBase
 {
+    /// <summary>
+    /// Gets or sets a value indicating whether the data type is nullable.
+    /// </summary>
+    public bool IsNullable { get; set; } = false;
+}
+
+/// <summary>
+/// Represents an unknown data type. Used when the data type name is not recognized.
+/// </summary>
+public sealed class UnknownDataType : DataTypeBase
+{
+    /// <summary>
+    /// Descriptor for the serialization of <see cref="UnknownDataType"/>.
+    /// </summary>
+    public const string Descriptor = "unknown";
+
+    /// <summary>
+    /// Constructs a new instance of <see cref="UnknownDataType"/>.
+    /// </summary>
+    /// <remarks>Meant for Deserialization purposes.</remarks>
+    public UnknownDataType() { }
+
+    /// <summary>
+    /// Constructs a new instance of <see cref="UnknownDataType"/> with a given <paramref name="typeName"/>.
+    /// </summary>
+    /// <param name="typeName">The name of the given type.</param>
+    public UnknownDataType(string typeName) => StoreType= typeName;
+
+    /// <summary>
+    /// Gets or sets the name of the type.
+    /// </summary>
+    public string StoreType { get; set; } = string.Empty;
 }
 
 /// <summary>
 /// Encapsulates simple data types - <see cref="SimpleType.SimpleType"/>.
 /// </summary>
-public class SimpleType : DataTypeBase
+public sealed class SimpleType : DataTypeBase
 {
     /// <summary>
     /// Descriptor for the serialization of simple data types.
     /// </summary>
     public const string Descriptor = "simple";
-    public enum Simpletype
+
+    /// <summary>
+    /// Constructs a new instance of <see cref="SimpleType"/>.
+    /// </summary>
+    /// <remarks>Meant for Deserialization purposes.</remarks>
+    public SimpleType() { }
+
+    /// <summary>
+    /// Constructs a new instance of <see cref="SimpleType"/> with a given <paramref name="type"/>.
+    /// </summary>
+    /// <param name="type">The type of the simple data type.</param>
+    public SimpleType(Types type) => Type = type;
+
+    /// <summary>
+    /// Represents the type of the simple data type.
+    /// </summary>
+    public enum Types
     {
+        TinyInteger,
+        SmallInteger,
         Integer,
-        Boolean,
+        BigInteger,
+        Money,
+        Float,
         Decimal,
+        Numeric, // TODO: consider creating a separate class for this type with precision and scale properties
+        Boolean,
         Date,
         Datetime,
+        Timestamp,
         Time,
     }
 
-    public Simpletype Type { get; set; }
+    /// <summary>
+    /// Gets or sets the type of the simple data type.
+    /// </summary>
+    public Types Type { get; set; }
 }
 
 /// <summary>
-/// Represents a string data type with a fixed length.
+/// Represents a Unicode string data type with a fixed length.
 /// </summary>
-public class NVarChar : DataTypeBase
+public sealed class NVarChar : DataTypeBase
 {
     /// <summary>
     /// Descriptor for the serialization of <see cref="NVarChar"/> data type.
     /// </summary>
     public const string Descriptor = "nVarChar";
+
+    /// <summary>
+    /// Constructs a new instance of <see cref="NVarChar"/>.
+    /// </summary>
+    /// <remarks>Meant for Deserialization purposes.</remarks>
+    public NVarChar() { }
+
+    /// <summary>
+    /// Constructs a new instance of <see cref="NVarChar"/> with a given <paramref name="lenght"/>.
+    /// </summary>
+    /// <param name="lenght">The type of the simple data type. Must be positive.</param>
+    /// <exception cref="ArgumentException">Thrown when the <paramref name="lenght"/> is not positive.</exception>
+    public NVarChar(int lenght)
+    {
+        if (lenght <= 0)
+        {
+            throw new ArgumentException("The length of the string must be positive.", nameof(lenght));
+        }
+
+        Lenght = lenght;
+    }
 
     /// <summary>
     /// Gets or sets the length limit of the string.
@@ -52,12 +134,62 @@ public class NVarChar : DataTypeBase
 
 
 /// <summary>
-/// Represents a string data type with a variable length.
+/// Represents a Unicode string data type with a variable length.
 /// </summary>
-public class VarChar : DataTypeBase
+public sealed class NVarCharMax : DataTypeBase
 {
     /// <summary>
-    /// Descriptor for the serialization of <see cref="VarChar"/> data type.
+    /// Descriptor for the serialization of <see cref="NVarCharMax"/> data type.
     /// </summary>
-    public const string Descriptor = "varChar";
+    public const string Descriptor = "nVarCharMax";
+}
+
+
+/// <summary>
+/// Represents a string data type with a variable length.
+/// </summary>
+public sealed class VarCharMax : DataTypeBase
+{
+    /// <summary>
+    /// Descriptor for the serialization of <see cref="VarCharMax"/> data type.
+    /// </summary>
+    public const string Descriptor = "varCharMax";
+}
+
+
+/// <summary>
+/// Represents a string data type with a fixed length.
+/// </summary>
+public sealed class VarChar : DataTypeBase
+{
+    /// <summary>
+    /// Descriptor for the serialization of <see cref="NVarChar"/> data type.
+    /// </summary>
+    public const string Descriptor = "nVarChar";
+
+    /// <summary>
+    /// Constructs a new instance of <see cref="VarChar"/>.
+    /// </summary>
+    /// <remarks>Meant for Deserialization purposes.</remarks>
+    public VarChar() { }
+
+    /// <summary>
+    /// Constructs a new instance of <see cref="VarChar"/> with a given <paramref name="lenght"/>.
+    /// </summary>
+    /// <param name="lenght">The type of the simple data type. Must be positive.</param>
+    /// <exception cref="ArgumentException">Thrown when the <paramref name="lenght"/> is not positive.</exception>
+    public VarChar(int lenght)
+    {
+        if (lenght <= 0)
+        {
+            throw new ArgumentException("The length of the string must be positive.", nameof(lenght));
+        }
+
+        Lenght = lenght;
+    }
+
+    /// <summary>
+    /// Gets or sets the length limit of the string.
+    /// </summary>
+    public int Lenght { get; set; }
 }
