ffd10ef88c49fc1f24764e6b5678153de22171f9
Michael Ševčík <69696161@cuni.cz>
2024-06-09 23:43:35 +0200

    Get DbModel in mapper component

--- src/Modules/DataIntegration/MapperPages/Mapper.razor.cs
+++ src/Modules/DataIntegration/MapperPages/Mapper.razor.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using BIManagement.Modules.DataIntegration.Domain.DbModelling;
+using BIManagement.Modules.Users.Api;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Http;
 using Microsoft.JSInterop;
 
 namespace BIManagement.Modules.DataIntegration.MapperComponent
@@ -7,26 +10,66 @@ namespace BIManagement.Modules.DataIntegration.MapperComponent
     {
         [Inject]
         IJSRuntime JSRuntime { get; set; } = default!;
-        MapperJsInterop? MapperJSInterop { get; set; }
+
+        [Inject]
+        ICostumerDbModelManager CostumerDbModelManager { get; set; } = default!;
+
+        [Inject]
+        IUserAccessor UserAccessor { get; set; } = default!;
+
+        [Inject]
+        IHttpContextAccessor HttpContextAccessor { get; set; } = default!;
+
+        MapperJsInterop? mapperJSInterop { get; set; }
+
+        DbModel? dbModel { get; set; }
+
+        /// <summary>
+        /// The Costumer id to use for the component.
+        /// Initialized either by the parent component or by the user id of the current Costumer.
+        /// </summary>
+        [Parameter]
+        public string? CostumerId { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (CostumerId == null)
+            {
+                var userIdResult = await UserAccessor.GetCostumerId(
+                    HttpContextAccessor.HttpContext ?? throw new InvalidOperationException("Http context is not accesible."));
+                if (userIdResult.IsFailure)
+                {
+                    throw new InvalidOperationException(userIdResult.Error.Message);
+                }
+
+                CostumerId = userIdResult.Value;
+            }
+        }
+
         protected override async Task OnAfterRenderAsync(bool isFirst)
         {
-            Console.WriteLine("OnAfterRenderAsync");
-            Console.WriteLine(isFirst);
-            //if (isFirst && MapperJSInterop == null && JSRuntime != null)
+            if (!isFirst)
             {
-                MapperJSInterop = new(JSRuntime); // TODO: Uncomment 
+                return;
+            }
+            
+            dbModel = await CostumerDbModelManager.GetAsync(CostumerId!);
+
+            if (mapperJSInterop == null && JSRuntime != null)
+            {
+                mapperJSInterop = new(JSRuntime); // TODO: Uncomment 
 
                 //create a variable  in the component and assign it a value from the JSInterop
-                var number = await MapperJSInterop.GetNumber();
+                var number = await mapperJSInterop.GetNumber();
                 Console.WriteLine(number);
             }
         }
 
         public async ValueTask DisposeAsync()
         {
-            if (MapperJSInterop != null)
+            if (mapperJSInterop != null)
             {
-                await MapperJSInterop.DisposeAsync();
+                await mapperJSInterop.DisposeAsync();
             }
         }
 
