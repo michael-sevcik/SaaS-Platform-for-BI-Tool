a0e44915c8acd9f84ad057afb69f69151313a8c6
Michael Ševčík <69696161@cuni.cz>
2024-06-12 17:42:03 +0200

    Add base repository class and use it

--- src/Modules/DataIntegration/Persistence/Repositories/CostumerDbModelRepository.cs
+++ src/Modules/DataIntegration/Persistence/Repositories/CostumerDbModelRepository.cs
@@ -1,7 +1,6 @@
 ﻿using BIManagement.Common.Application.ServiceLifetimes;
 using BIManagement.Common.Shared.Results;
 using BIManagement.Modules.DataIntegration.Domain.DbModelling;
-using BIManagement.Modules.DataIntegration.Domain.Errors;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
@@ -11,19 +10,19 @@ namespace BIManagement.Modules.DataIntegration.Persistence.Repositories;
 /// Default implementation of <see cref="ICostumerDbModelRepository"/>.
 /// </summary>
 /// <param name="dbContext">The context of database in which costumer db models are stored.</param>
-internal class CostumerDbModelRepository(ILogger<CostumerDbModelRepository> logger, DataIntegrationDbContext dbContext)
-    : ICostumerDbModelRepository, IScoped
+/// <inheritdoc/>
+internal class CostumerDbModelRepository(ILogger<CostumerDbModelRepository> logger, DataIntegrationDbContext dbContext) :
+    DataIntegrationBaseRepository<CostumerDbModelRepository, CostumerDbModel>(logger, dbContext),
+    ICostumerDbModelRepository,
+    IScoped
 {
-    private readonly DbSet<CostumerDbModel> costumerDbModels
-        = dbContext.Set<CostumerDbModel>();
-
     /// <inheritdoc/>
     public async Task<Result> SaveAsync(CostumerDbModel costumerDbModel)
     {
         Result result;
 
         // check if it exists and update it
-        if (costumerDbModels.Any(model => model.CostumerId == costumerDbModel.CostumerId))
+        if (entities.Any(model => model.CostumerId == costumerDbModel.CostumerId))
         {
             result = await UpdateAsync(costumerDbModel);
         }
@@ -33,46 +32,11 @@ internal class CostumerDbModelRepository(ILogger<CostumerDbModelRepository> logg
             result = await AddAsync(costumerDbModel);
         }
 
-        costumerDbModels.Entry(costumerDbModel).State = EntityState.Detached;
+        entities.Entry(costumerDbModel).State = EntityState.Detached;
         return result;
     }
 
     /// <inheritdoc/>
     public async Task<CostumerDbModel?> GetAsync(string costumerId)
-        => await costumerDbModels.AsNoTracking().SingleOrDefaultAsync(model => model.CostumerId == costumerId);
-
-    /// <summary>
-    /// Asynchronously adds a new <see cref="CostumerDbModel"/> to the repository.
-    /// </summary>
-    /// <param name="costumerDbModel">The model to update.</param>
-    /// <returns>
-    /// Task object that represents the asynchronous operation.
-    /// The Value property returns instance of <see cref="Result"/> that represents the result
-    /// of the operation.
-    /// </returns>
-    private async Task<Result> UpdateAsync(CostumerDbModel costumerDbModel)
-    {
-        costumerDbModels.Update(costumerDbModel);
-        return await SaveChangesAsync();
-    }
-
-    private async Task<Result> AddAsync(CostumerDbModel costumerDbModel)
-    {
-        await costumerDbModels.AddAsync(costumerDbModel);
-        return await SaveChangesAsync();
-    }
-
-    private async Task<Result> SaveChangesAsync()
-    {
-        try
-        {
-            await dbContext.SaveChangesAsync();
-            return Result.Success();
-        }
-        catch (Exception ex)
-        {
-            logger.LogError(ex, "Saving changes to database failed");
-            return Result.Failure(RepositoryErrors.DatabaseOperationFailed);
-        }
-    }
+        => await entities.AsNoTracking().SingleOrDefaultAsync(model => model.CostumerId == costumerId);
 }
---
+++ src/Modules/DataIntegration/Persistence/Repositories/DataIntegrationBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using BIManagement.Common.Persistence.Repositories;
+using BIManagement.Common.Shared.Errors;
+using BIManagement.Modules.DataIntegration.Domain.Errors;
+using Microsoft.Extensions.Logging;
+
+namespace BIManagement.Modules.DataIntegration.Persistence.Repositories;
+
+/// <summary>
+/// Base Repository for Data Integration module.
+/// </summary>
+/// <inheritdoc/>
+internal abstract class DataIntegrationBaseRepository<TDerived, TEntity>(ILogger<TDerived> logger, DataIntegrationDbContext dbContext) :
+    BaseRepository<TDerived, TEntity, DataIntegrationDbContext>(logger, dbContext)
+    where TEntity : class
+{
+    protected override Error DatabaseOperationFailedError => RepositoryErrors.DatabaseOperationFailed;
+
+}
