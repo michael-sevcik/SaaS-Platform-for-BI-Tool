c1c706e67b014498d012b737a824d9d46ea5faeb
Michael Ševčík <69696161@cuni.cz>
2024-06-12 17:39:52 +0200

    Add domain objects schema mapping and target database table

---
+++ src/Modules/DataIntegration/Domain/Mapping/SchemaMapping.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BIManagement.Modules.DataIntegration.Domain.Mapping;
+
+/// <summary>
+/// Encapsulates the <see cref="Mapping"/> of a given <see cref="TargetDbTable"/> to tables from database of costumer with <see cref="CostumerId"/>.
+/// </summary>
+public class SchemaMapping
+{
+    /// <summary>
+    /// Gets or sets the identifier of a costumer.
+    /// </summary>
+    [Required]
+    public string CostumerId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the identifier of a <see cref="TargetDbTable"/>.
+    /// </summary>
+    [Required]
+    public int TargetDbTableId { get; set; }
+
+
+    /// <summary>
+    /// Gets or sets the mapping of a <see cref="TargetDbTable"/> to tables from database of costumer with <see cref="CostumerId"/>.
+    /// </summary>
+    [Required]
+    public string Mapping { get; set; } = string.Empty;
+}
---
+++ src/Modules/DataIntegration/Domain/Mapping/TargetDbTable.cs
@@ -0,0 +1,33 @@
+﻿using BIManagement.Modules.DataIntegration.Domain.DbModelling;
+using System.ComponentModel.DataAnnotations;
+
+namespace BIManagement.Modules.DataIntegration.Domain.Mapping;
+
+/// <summary>
+/// Encapsulates the model of a target database table.
+/// </summary>
+public class TargetDbTable
+{
+    /// <summary>
+    /// Gets or sets the identifier of this entity.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the schema of a table <see cref="TableModel"/>.
+    /// </summary>
+    [Required]
+    public string Schema { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the name of a table <see cref="TableModel"/>.
+    /// </summary>
+    [Required]
+    public string TableName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the model of a table from target database.
+    /// </summary>
+    [Required]
+    public Table TableModel { get; set; } = default!;
+}
