ac641695e7221b3f25884c06a808c2176dd37d80
Michael Ševčík <69696161@cuni.cz>
2024-06-12 17:39:01 +0200

    Add documentation

--- src/Modules/DataIntegration/Domain/Errors/RepositoryErrors.cs
+++ src/Modules/DataIntegration/Domain/Errors/RepositoryErrors.cs
@@ -2,9 +2,16 @@
 
 namespace BIManagement.Modules.DataIntegration.Domain.Errors;
 
+/// <summary>
+/// Encapsulates errors that can occur during database interaction.
+/// </summary>
 public static class RepositoryErrors
 {
     public const string Namespace = "DataIntegration";
+
+    /// <summary>
+    /// Represents a situation when an operation executed on a database failed.
+    /// </summary>
     public static readonly Error DatabaseOperationFailed = new(
         $"{Namespace}.DatabaseOperationFailed",
         "Saving changes failed.");
