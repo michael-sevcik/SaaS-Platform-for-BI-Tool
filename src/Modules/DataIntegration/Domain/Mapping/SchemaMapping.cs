fc4075233792e9cb7c53554368e167f32015f8d6
Michael Ševčík <69696161@cuni.cz>
2024-06-13 23:51:44 +0200

    Add property indicating whether schema mapping is complete

--- src/Modules/DataIntegration/Domain/Mapping/SchemaMapping.cs
+++ src/Modules/DataIntegration/Domain/Mapping/SchemaMapping.cs
@@ -10,19 +10,23 @@ public class SchemaMapping
     /// <summary>
     /// Gets or sets the identifier of a costumer.
     /// </summary>
-    [Required]
-    public string CostumerId { get; set; } = string.Empty;
+    [Required] public string CostumerId { get; set; } = string.Empty;
 
     /// <summary>
     /// Gets or sets the identifier of a <see cref="TargetDbTable"/>.
     /// </summary>
-    [Required]
-    public int TargetDbTableId { get; set; }
+    [Required] public int TargetDbTableId { get; set; }
 
+    /// <summary>
+    /// Gets or sets the flag indicating whether the mapping is complete.
+    /// </summary>
+    /// <remarks>
+    /// Indicates whether all non-nullable columns are mapped.
+    /// </remarks>
+    [Required] public bool IsComplete { get; set; }
 
     /// <summary>
     /// Gets or sets the mapping of a <see cref="TargetDbTable"/> to tables from database of costumer with <see cref="CostumerId"/>.
     /// </summary>
-    [Required]
-    public string Mapping { get; set; } = string.Empty;
+    [Required] public string Mapping { get; set; } = string.Empty;
 }
---
+++ src/Modules/DataIntegration/Persistence/Migrations/20240613214934_AddIsSchemaMappingCompleteProperty.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BIManagement.Modules.DataIntegration.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsSchemaMappingCompleteProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsComplete",
+                schema: "dataIntegration",
+                table: "SchemaMappings",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsComplete",
+                schema: "dataIntegration",
+                table: "SchemaMappings");
+        }
+    }
+}
