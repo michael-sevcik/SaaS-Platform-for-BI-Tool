2e77ef2cc8b850a870bb839f988ac02f0a954329
Michael Ševčík <69696161@cuni.cz>
2024-07-13 16:05:17 +0200

    Convert to regular class to allow instanciating through configuration

--- src/Modules/Deployment/Domain/Configuration/MetabaseConfiguration.cs
+++ src/Modules/Deployment/Domain/Configuration/MetabaseConfiguration.cs
@@ -45,12 +45,41 @@ public enum SmtpSecurity
 /// <summary>
 /// Represents the SMTP settings.
 /// </summary>
-/// <param name="Host">The SMTP host.</param>
-/// <param name="Port">The SMTP port.</param>
-/// <param name="Security">The SMTP security option.</param>
-/// <param name="Username">The username for SMTP authentication.</param>
-/// <param name="Password">The password for SMTP authentication.</param>
-public record SmtpSettings(string Host, int Port, SmtpSecurity Security, string Username, string Password);
+public class SmtpSettings
+{
+    public string Host { get; init; } = string.Empty;
+    public int Port {get; init; }
+    public SmtpSecurity Security { get; init; } = SmtpSecurity.None;
+    public string Username { get; init; } = string.Empty;
+    public string Password { get; init; } = string.Empty;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SmtpSettings"/> class.
+    /// </summary>
+    /// <param name="host">The SMTP host.</param>
+    /// <param name="port">The SMTP port.</param>
+    /// <param name="security">The SMTP security option.</param>
+    /// <param name="username">The username for SMTP authentication.</param>
+    /// <param name="password">The password for SMTP authentication.</param>
+    public SmtpSettings(string host, int port, SmtpSecurity security, string username, string password)
+    {
+        this.Host = host;
+        this.Port = port;
+        this.Security = security;
+        this.Username = username;
+        this.Password = password;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SmtpSettings"/> class.
+    /// </summary>
+    /// <remarks>
+    /// Used for options binding.
+    /// </remarks>
+    public SmtpSettings()
+    {
+    }
+};
 
 /// <summary>
 /// Represents the configuration for Metabase.
