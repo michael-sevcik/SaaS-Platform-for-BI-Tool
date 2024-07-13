26e13d26dbe8c98bfced7fc6c0b38cbf2287cef7
Michael Ševčík <69696161@cuni.cz>
2024-07-13 11:42:19 +0200

    Update doc and imports

--- src/Modules/Users/Domain/IIntegrationNotifier.cs
+++ src/Modules/Users/Domain/IIntegrationNotifier.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace BIManagement.Modules.Users.Domain;
 
-namespace BIManagement.Modules.Users.Domain;
-
+/// <summary>
+/// Represents a service that notifies other modules about the user deletion.
+/// </summary>
 public interface IIntegrationNotifier
 {
     /// <summary>
