6bbd1778fd1a65a2d11dbb43636eb8deb49ba0a3
Michael Ševčík <69696161@cuni.cz>
2025-01-08 12:16:22 +0100

    Add keycloak

--- src/Modules/Users/Infrastructure/ServiceInstallers/IdentityServiceInstaller.cs
+++ src/Modules/Users/Infrastructure/ServiceInstallers/IdentityServiceInstaller.cs
@@ -12,6 +12,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using BIManagement.Common.Persistence.Extensions;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
 
 namespace BIManagement.Modules.Users.Infrastructure.ServiceInstallers
 {
@@ -24,12 +27,39 @@ namespace BIManagement.Modules.Users.Infrastructure.ServiceInstallers
             services.AddScoped<IdentityRedirectManager>();
             services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
 
+            var keycloackConfig = configuration.GetSection("Modules:Users:Keycloak");
+
             services.AddAuthentication(options =>
             {
                 options.DefaultScheme = IdentityConstants.ApplicationScheme;
                 options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
             })
+                .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, "Keycloak", options =>
+                {
+                    options.Authority = keycloackConfig["Authority"];
+                    options.ClientId = keycloackConfig["ClientId"];
+                    options.ClientSecret = keycloackConfig["ClientSecret"];
+                    options.ResponseType = OpenIdConnectResponseType.Code;
+                    options.SaveTokens = true;
+                    options.MetadataAddress = $"{options.Authority}/.well-known/openid-configuration";
+                    
+                    // dev only
+                    options.RequireHttpsMetadata = false;
+                    options.GetClaimsFromUserInfoEndpoint = true;
+
+                    options.Scope.Add("openid");
+                    options.Scope.Add("profile");
+                    options.Scope.Add("email");
+                    options.Scope.Add("roles");
+
+                    //options.TokenValidationParameters = new TokenValidationParameters
+                    //{
+                    //    ValidateIssuer = true
+                    //};
+                })
                 .AddIdentityCookies();
+                
+
 
             services.AddDbContext<UsersContext>((serviceProvider, options) => {
                 ConnectionStringOptions connectionString = serviceProvider.GetService<IOptions<ConnectionStringOptions>>()?.Value
